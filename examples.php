9e8ab98fc7f97a0f451b7868838c0d730270a4af
Dave Ross <dave@davidmichaelross.com>
2015-03-31 16:58:12 -0400

    Initial commit

---
+++ examples.php
@@ -0,0 +1,6 @@
+<?php
+
+include 'boston_neighborhoods.php';
+echo 'Boston City Hall is in ' . com\davidmichaelross\boston_neighborhoods\get_neighborhood( - 71.0580, 42.3603 ) . "\n";
+echo 'Massachusetts General Hospital is in ' . com\davidmichaelross\boston_neighborhoods\get_neighborhood( - 71.0686, 42.3628 ) . "\n";
+echo 'Faneuil Hall is in ' . com\davidmichaelross\boston_neighborhoods\get_neighborhood( - 71.0568, 42.3600 ) . "\n";
